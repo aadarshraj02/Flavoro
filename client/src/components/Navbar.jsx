fdd744aa9ba5c4db514033743bd17646e0ce323c
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-24 19:40:31 +0530

    added input to search

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,7 +5,14 @@ const Navbar = () => {
         <h3 className="">{new Date().toUTCString().slice(0, 16)}</h3>
         <h1>Flavoro Foods</h1>
       </div>
-      <div></div>
+      <div>
+        <input
+          type="search"
+          name="search"
+          id="search"
+          placeholder="Search foods..."
+        />
+      </div>
     </nav>
   );
 };
