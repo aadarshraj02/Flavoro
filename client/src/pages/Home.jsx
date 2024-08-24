0d2c8a51ab7cd6e48ec6ccbfd08c9da88afca886
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-24 19:58:57 +0530

    imported categoryMenu

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import CategoryMenu from "../components/CategoryMenu";
 import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
     <>
       <Navbar />
+      <CategoryMenu />
     </>
   );
 };
