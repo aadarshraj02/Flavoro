0b049aba508b9783bf80a493a712b9692b555b1d
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-24 19:57:27 +0530

    styled navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,15 @@
 const Navbar = () => {
   return (
-    <nav>
+    <nav className="flex flex-col md:flex-row justify-between items-center px-6 py-3 gap-3">
       <div>
-        <h3 className="">{new Date().toUTCString().slice(0, 16)}</h3>
-        <h1>Flavoro Foods</h1>
+        <h3 className="text-xl font-bold text-gray-600">
+          {new Date().toUTCString().slice(0, 16)}
+        </h3>
+        <h1 className="text-2xl font-bold mt-2">Flavoro Foods</h1>
       </div>
       <div>
         <input
+          className="p-3 border border-gray-400 text-sm rounded-lg outline-none w-[80vw] md:w-[30vw]"
           type="search"
           name="search"
           id="search"
