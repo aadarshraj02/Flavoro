ddce19339c9dbd255412147249906af198ec7f59
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-24 20:42:02 +0530

    updated font sizer of h3

--- client/src/components/CategoryMenu.jsx
+++ client/src/components/CategoryMenu.jsx
@@ -1,7 +1,7 @@
 const CategoryMenu = () => {
   return (
     <div className="ml-6">
-      <h3>Find the best Food</h3>
+      <h3 className="text-xl font-semibold">Find the best Food</h3>
       <div className="my-5 flex gap-3 overflow-x-scroll scroll-smooth sm:overflow-x-hidden">
         <button className="px-3 py-2 text-center bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white">
           All
