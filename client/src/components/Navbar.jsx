2fa731bca5dd24453308b93fcef03b576ec1ab19
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-24 19:39:01 +0530

    added header

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,6 +3,7 @@ const Navbar = () => {
     <nav>
       <div>
         <h3 className="">{new Date().toUTCString().slice(0, 16)}</h3>
+        <h1>Flavoro Foods</h1>
       </div>
       <div></div>
     </nav>
