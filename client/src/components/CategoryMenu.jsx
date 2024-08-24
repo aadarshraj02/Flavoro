21dfdf661ad0b3afeb4ea7e4f469a65bec7dea1b
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-24 20:12:16 +0530

    removed all buttons

--- client/src/components/CategoryMenu.jsx
+++ client/src/components/CategoryMenu.jsx
@@ -3,11 +3,7 @@ const CategoryMenu = () => {
     <div>
       <h3>Find the best Food</h3>
       <div>
-        <button>All</button>
-        <button>Lunch</button>
-        <button>Breakfast</button>
-        <button>Dinner</button>
-        <button>Snacks</button>
+     
       </div>
     </div>
   );
