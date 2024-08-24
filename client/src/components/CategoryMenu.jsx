fcf37d9a8cb8adeb1a316666c178c083512c1b60
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-24 20:31:38 +0530

    added filtered menu buttons

--- client/src/components/CategoryMenu.jsx
+++ client/src/components/CategoryMenu.jsx
@@ -3,7 +3,21 @@ const CategoryMenu = () => {
     <div>
       <h3>Find the best Food</h3>
       <div>
-     
+        <button className="px-3 py-2 text-center bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white">
+          All
+        </button>
+        <button className="px-3 py-2 text-center bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white">
+          Lunch
+        </button>
+        <button className="px-3 py-2 text-center bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white">
+          Breakfast
+        </button>
+        <button className="px-3 py-2 text-center bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white">
+          Dinner
+        </button>
+        <button className="px-3 py-2 text-center bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white">
+          Snacks
+        </button>
       </div>
     </div>
   );
