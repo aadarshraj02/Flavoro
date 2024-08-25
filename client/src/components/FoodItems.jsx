a8e1702729ffd9b57e6820a640bffb80f0e33fad
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-25 21:43:42 +0530

    passed id,name,image,price,desc and rating as props

--- client/src/components/FoodItems.jsx
+++ client/src/components/FoodItems.jsx
@@ -5,7 +5,15 @@ const FoodItems = () => {
   return (
     <div>
       {foodData.map((food) => (
-        <FoodCard key={food.id} />
+        <FoodCard
+          key={food.id}
+          id={food.id}
+          name={food.name}
+          price={food.price}
+          desc={food.rating}
+          rating={food.rating}
+          image={food.img}
+        />
       ))}
     </div>
   );
