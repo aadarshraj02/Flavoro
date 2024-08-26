5691da8acb382c9a8dc06bfcf1abcf9bc33fa73e
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-26 20:22:30 +0530

    imported CartSlice and added as reducer

--- client/src/redux/Store.js
+++ client/src/redux/Store.js
@@ -1,5 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import CartSlice from "./slices/CartSlice";
 
-const Store = configureStore({});
+const Store = configureStore({
+  reducer: {
+    cart: CartSlice,
+  },
+});
 
 export default Store;
