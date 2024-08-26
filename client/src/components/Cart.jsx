bb948bfac52fc3c977ab6bbe5e34cc8a50da4548
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-26 18:03:32 +0530

    updated responive design fpr cart

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -5,11 +5,16 @@ import ItemCard from "./ItemCard";
 const Cart = () => {
   return (
     <>
-      <div className="fixed right-0 top-0 w-full sm:w-[60vw] md:w-[40vw] lg:w-[20vw] bg-white h-full p-5">
+      <div className="fixed right-0 top-0 w-full sm:w-[60vw] md:w-[40vw] lg:w-[25vw] 2xl:w-[20vw] bg-white h-full p-5">
         <div className="flex justify-between items-center my-3 ">
           <h2 className="text-2xl font-bold text-gray-700">My Orders</h2>
           <IoCloseSharp className="border-2 border-gray-600 text-gray-600 font-bold p-1 text-2xl rounded-md hover:text-red-500 hover:border-red-500 cursor-pointer" />
         </div>
+        <ItemCard />
+        <ItemCard />
+        <ItemCard />
+        <ItemCard />
+        <ItemCard />
         <ItemCard />
         <div className="absolute bottom-0 ">
           <h3 className="font-semibold text-gray-700">Items:</h3>
