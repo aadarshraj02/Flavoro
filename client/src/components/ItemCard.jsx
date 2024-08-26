685467e4fe77d3789cf10871a5b1dc8dda6aedb7
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-26 18:00:29 +0530

    styled itemsCard

--- client/src/components/ItemCard.jsx
+++ client/src/components/ItemCard.jsx
@@ -3,14 +3,17 @@ import { HiMinusSm } from "react-icons/hi";
 
 const ItemCard = () => {
   return (
-    <div>
+    <div className="flex gap-2 shadow-md rounded-lg p-2">
       <img src="" alt="" className="w-[50px] h-[50px]" />
       <div>
-        <h2>Onion Pizza</h2>
-        <div>
-          <GoPlus className="border-2 border-gray-600 text-gray-600 hover:text-white hover:bg-green-500 rounded-md p-1 text-2xl transition-all duration-300 ease-linear cursor-pointer font-semibold hover:border-none" />
-          <span>1</span>
-          <HiMinusSm className="border-2 border-gray-600 text-gray-600 hover:text-white hover:bg-green-500 rounded-md p-1 text-2xl transition-all duration-300 ease-linear cursor-pointer font-semibold hover:border-none" />
+        <h2 className="font-bold text-gray-700">Onion Pizza</h2>
+        <div className="flex justify-between items-center">
+          <h3 className="text-green-500 font-bold">₹120</h3>
+          <div className="flex justify-center items-center gap-2 absolute right-7">
+            <GoPlus className="border-2 border-gray-600 text-gray-600 hover:text-white hover:bg-green-500 rounded-md p-1 text-2xl transition-all duration-300 ease-linear cursor-pointer font-semibold hover:border-none" />
+            <span>1</span>
+            <HiMinusSm className="border-2 border-gray-600 text-gray-600 hover:text-white hover:bg-green-500 rounded-md p-1 text-2xl transition-all duration-300 ease-linear cursor-pointer font-semibold hover:border-none" />
+          </div>
         </div>
       </div>
     </div>
