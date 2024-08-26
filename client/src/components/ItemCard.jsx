3b039b3d8c0f1de79aa37a6b13c225b50c737525
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-26 17:52:06 +0530

    added icons and styled icons

--- client/src/components/ItemCard.jsx
+++ client/src/components/ItemCard.jsx
@@ -1,5 +1,20 @@
+import { GoPlus } from "react-icons/go";
+import { HiMinusSm } from "react-icons/hi";
+
 const ItemCard = () => {
-  return <div></div>;
+  return (
+    <div>
+      <img src="" alt="" className="w-[50px] h-[50px]" />
+      <div>
+        <h2>Onion Pizza</h2>
+        <div>
+          <GoPlus className="border-2 border-gray-600 text-gray-600 hover:text-white hover:bg-green-500 rounded-md p-1 text-2xl transition-all duration-300 ease-linear cursor-pointer font-semibold hover:border-none" />
+          <span>1</span>
+          <HiMinusSm className="border-2 border-gray-600 text-gray-600 hover:text-white hover:bg-green-500 rounded-md p-1 text-2xl transition-all duration-300 ease-linear cursor-pointer font-semibold hover:border-none" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ItemCard;
