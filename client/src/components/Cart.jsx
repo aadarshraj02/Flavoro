72c55729b4f3f15d3cc232bb828715f3fa53a53d
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-27 14:00:58 +0530

    added toggle to close cart

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -2,17 +2,27 @@ import { IoCloseSharp } from "react-icons/io5";
 import { MdOutlineShoppingCartCheckout } from "react-icons/md";
 import ItemCard from "./ItemCard";
 import { useSelector } from "react-redux";
+import { useState } from "react";
 
 const Cart = () => {
+  const [activeCart, setActiveCart] = useState(true);
+
   const cartItems = useSelector((state) => state.cart.cart);
   console.log(cartItems);
 
   return (
     <>
-      <div className="fixed right-0 top-0 w-full sm:w-[60vw] md:w-[40vw] lg:w-[25vw] 2xl:w-[20vw] bg-white h-full p-5">
+      <div
+        className={`fixed right-0 top-0 w-full sm:w-[60vw] md:w-[40vw] lg:w-[25vw] 2xl:w-[20vw] bg-white h-full p-5 ${
+          activeCart ? "translate-x-0" : "translate-x-full"
+        }`}
+      >
         <div className="flex justify-between items-center my-3 ">
           <h2 className="text-2xl font-bold text-gray-700">My Orders</h2>
-          <IoCloseSharp className="border-2 border-gray-600 text-gray-600 font-bold p-1 text-2xl rounded-md hover:text-red-500 hover:border-red-500 cursor-pointer" />
+          <IoCloseSharp
+            onClick={() => setActiveCart(!activeCart)}
+            className="border-2 border-gray-600 text-gray-600 font-bold p-1 text-2xl rounded-md hover:text-red-500 hover:border-red-500 cursor-pointer"
+          />
         </div>
         <ItemCard />
         <div className="absolute bottom-0 ">
