9d25380ce2499a5e6053b459ce6dfb84fbbe5c06
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-01 22:28:37 +0530

    fixed error on increment and decrement

--- client/src/components/ItemCard.jsx
+++ client/src/components/ItemCard.jsx
@@ -24,13 +24,15 @@ const ItemCard = ({ id, name, qty, price, img }) => {
           <h3 className="text-green-500 font-bold">₹{price}</h3>
           <div className="flex justify-center items-center gap-2 absolute right-7">
             <GoPlus
-              onClick={() => dispatch(incrementQty({ id }))}
+              onClick={() =>
+                qty >= 1 ? dispatch(incrementQty({ id })) : (qty = 0)
+              }
               className="border-2 border-gray-600 text-gray-600 hover:text-white hover:bg-green-500 rounded-md p-1 text-2xl transition-all duration-300 ease-linear cursor-pointer font-semibold hover:border-none"
             />
             <span>{qty}</span>
             <HiMinusSm
               onClick={() =>
-                qty >= 1 ? dispatch(decrementQty({ id })) : (qty = 0)
+                qty > 1 ? dispatch(decrementQty({ id })) : (qty = 0)
               }
               className="border-2 border-gray-600 text-gray-600 hover:text-white hover:bg-green-500 rounded-md p-1 text-2xl transition-all duration-300 ease-linear cursor-pointer font-semibold hover:border-none"
             />
