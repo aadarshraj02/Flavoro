e14f367f6996e56d2f37af0ae835142d53fe9ed5
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-01 22:21:04 +0530

    added increment and decrement qty on buttons

--- client/src/components/ItemCard.jsx
+++ client/src/components/ItemCard.jsx
@@ -2,7 +2,11 @@ import { GoPlus } from "react-icons/go";
 import { HiMinusSm } from "react-icons/hi";
 import { MdDelete } from "react-icons/md";
 import { useDispatch } from "react-redux";
-import { removeFromCart } from "../redux/slices/CartSlice";
+import {
+  removeFromCart,
+  incrementQty,
+  decrementQty,
+} from "../redux/slices/CartSlice";
 
 const ItemCard = ({ id, name, qty, price, img }) => {
   const dispatch = useDispatch();
@@ -19,9 +23,15 @@ const ItemCard = ({ id, name, qty, price, img }) => {
         <div className="flex justify-between items-center">
           <h3 className="text-green-500 font-bold">₹{price}</h3>
           <div className="flex justify-center items-center gap-2 absolute right-7">
-            <GoPlus className="border-2 border-gray-600 text-gray-600 hover:text-white hover:bg-green-500 rounded-md p-1 text-2xl transition-all duration-300 ease-linear cursor-pointer font-semibold hover:border-none" />
+            <GoPlus
+              onClick={incrementQty}
+              className="border-2 border-gray-600 text-gray-600 hover:text-white hover:bg-green-500 rounded-md p-1 text-2xl transition-all duration-300 ease-linear cursor-pointer font-semibold hover:border-none"
+            />
             <span>{qty}</span>
-            <HiMinusSm className="border-2 border-gray-600 text-gray-600 hover:text-white hover:bg-green-500 rounded-md p-1 text-2xl transition-all duration-300 ease-linear cursor-pointer font-semibold hover:border-none" />
+            <HiMinusSm
+              onClick={decrementQty}
+              className="border-2 border-gray-600 text-gray-600 hover:text-white hover:bg-green-500 rounded-md p-1 text-2xl transition-all duration-300 ease-linear cursor-pointer font-semibold hover:border-none"
+            />
           </div>
         </div>
       </div>
