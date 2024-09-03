f7c89126db437502618cb546169d420da648f977
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-03 21:03:53 +0530

    wrapped in react Fragments

--- client/src/components/FoodItems.jsx
+++ client/src/components/FoodItems.jsx
@@ -1,21 +1,24 @@
 import FoodCard from "./FoodCard";
 import foodData from "../data/foodData";
+import toast, { Toaster } from "react-hot-toast";
 
 const FoodItems = () => {
   return (
-    <div className="flex flex-wrap gap-10 justify-center md:justify-start mx-6 my-10">
-      {foodData.map((food) => (
-        <FoodCard
-          key={food.id}
-          id={food.id}
-          name={food.name}
-          price={food.price}
-          desc={food.desc}
-          rating={food.rating}
-          img={food.img}
-        />
-      ))}
-    </div>
+    <>
+      <div className="flex flex-wrap gap-10 justify-center md:justify-start mx-6 my-10">
+        {foodData.map((food) => (
+          <FoodCard
+            key={food.id}
+            id={food.id}
+            name={food.name}
+            price={food.price}
+            desc={food.desc}
+            rating={food.rating}
+            img={food.img}
+          />
+        ))}
+      </div>
+    </>
   );
 };
 
