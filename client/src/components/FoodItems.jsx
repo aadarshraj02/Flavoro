cad3dcc68ece83a30ce0f691924f08f82bf57011
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-03 21:07:06 +0530

    added function to handle react toast on adding item

--- client/src/components/FoodItems.jsx
+++ client/src/components/FoodItems.jsx
@@ -3,8 +3,13 @@ import foodData from "../data/foodData";
 import toast, { Toaster } from "react-hot-toast";
 
 const FoodItems = () => {
+  const handleToast = () => {
+    toast.success("Item added to cart");
+  };
+
   return (
     <>
+      <Toaster position="top-center" reverseOrder="false" />
       <div className="flex flex-wrap gap-10 justify-center md:justify-start mx-6 my-10">
         {foodData.map((food) => (
           <FoodCard
@@ -15,6 +20,7 @@ const FoodItems = () => {
             desc={food.desc}
             rating={food.rating}
             img={food.img}
+            handleToast={handleToast}
           />
         ))}
       </div>
