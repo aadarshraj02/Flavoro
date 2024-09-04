02b8f6b8e197ec18a188fc20bbae73929ee6febc
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-04 22:19:29 +0530

    added loading effect on order successful

--- client/src/pages/Success.jsx
+++ client/src/pages/Success.jsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from "react";
+import { PropagateLoader } from "react-spinners";
+
 const Success = () => {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h2 className="text-3xl font-semibold mb-4 ">Order Successful!!</h2>
-      <p>Your Order has been placed Successfully</p>
+      {loading ? (
+        <PropagateLoader />
+      ) : (
+        <div>
+          <h2 className="text-3xl font-semibold mb-4 ">Order Successful!!</h2>
+          <p>Your Order has been placed Successfully</p>
+        </div>
+      )}
     </div>
   );
 };
