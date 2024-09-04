41c4160be9f3d3206de691716db8771d49f85da2
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-04 21:02:43 +0530

    displayed food data according to category

--- client/src/components/FoodItems.jsx
+++ client/src/components/FoodItems.jsx
@@ -1,8 +1,11 @@
 import FoodCard from "./FoodCard";
 import foodData from "../data/foodData";
 import toast, { Toaster } from "react-hot-toast";
+import { useSelector } from "react-redux";
 
 const FoodItems = () => {
+  const category = useSelector((state) => state.category.category);
+
   const handleToast = () => {
     toast.success("Item added to cart");
   };
@@ -11,18 +14,26 @@ const FoodItems = () => {
     <>
       <Toaster position="top-center" reverseOrder="false" />
       <div className="flex flex-wrap gap-10 justify-center md:justify-start mx-6 my-10">
-        {foodData.map((food) => (
-          <FoodCard
-            key={food.id}
-            id={food.id}
-            name={food.name}
-            price={food.price}
-            desc={food.desc}
-            rating={food.rating}
-            img={food.img}
-            handleToast={handleToast}
-          />
-        ))}
+        {foodData
+          .filter((food) => {
+            if (category === "All") {
+              return food;
+            } else {
+              return category === food.category;
+            }
+          })
+          .map((food) => (
+            <FoodCard
+              key={food.id}
+              id={food.id}
+              name={food.name}
+              price={food.price}
+              desc={food.desc}
+              rating={food.rating}
+              img={food.img}
+              handleToast={handleToast}
+            />
+          ))}
       </div>
     </>
   );
