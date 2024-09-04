e71bbd99309a10f9e4f956ffe828b24c7c6b0b94
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-04 22:00:53 +0530

    added default successful page

--- client/src/pages/Success.jsx
+++ client/src/pages/Success.jsx
@@ -1,5 +1,10 @@
 const Success = () => {
-  return <div></div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h2>Order Successful!!</h2>
+      <p>Your Order has been placed Successfully</p>
+    </div>
+  );
 };
 
 export default Success;
