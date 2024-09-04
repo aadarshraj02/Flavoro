b844762903953b9d23c0a00530c4414cd34cb4ff
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-04 21:28:34 +0530

    added SearchSlices

--- client/src/redux/Store.js
+++ client/src/redux/Store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import CartSlice from "./slices/CartSlice";
 import CategorySlices from "./slices/CategorySlices";
+import SearchSlices from "./slices/SearchSlices.jsx";
 
 const Store = configureStore({
   reducer: {
     cart: CartSlice,
     category: CategorySlices,
+    search: SearchSlices,
   },
 });
 
