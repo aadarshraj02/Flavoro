d211e9a908fd9d8800612be0ad6b1caf95a73bce
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-04 20:24:15 +0530

    added categories dynaically

--- client/src/components/CategoryMenu.jsx
+++ client/src/components/CategoryMenu.jsx
@@ -22,18 +22,14 @@ const CategoryMenu = () => {
         <button className="px-3 py-2 text-center bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white">
           All
         </button>
-        <button className="px-3 py-2 text-center bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white">
-          Lunch
-        </button>
-        <button className="px-3 py-2 text-center bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white">
-          Breakfast
-        </button>
-        <button className="px-3 py-2 text-center bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white">
-          Dinner
-        </button>
-        <button className="px-3 py-2 text-center bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white">
-          Snacks
-        </button>
+        {categories.map((category, index) => (
+          <button
+            key={index}
+            className="px-3 py-2 text-center bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white"
+          >
+            {category}
+          </button>
+        ))}
       </div>
     </div>
   );
