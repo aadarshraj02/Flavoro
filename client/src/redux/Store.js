58a167d0fe424c3219d46aaf9e97b9c69c0e43b9
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-04 20:30:32 +0530

    added categorySlice in store reducer

--- client/src/redux/Store.js
+++ client/src/redux/Store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import CartSlice from "./slices/CartSlice";
+import CategorySlices from "./slices/CategorySlices";
 
 const Store = configureStore({
   reducer: {
     cart: CartSlice,
+    category: CategorySlices,
   },
 });
 
