2e296bec430f6795faa8d1f9f1edad624bc067c3
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-04 22:05:52 +0530

    styled h2

--- client/src/pages/Success.jsx
+++ client/src/pages/Success.jsx
@@ -1,7 +1,7 @@
 const Success = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h2>Order Successful!!</h2>
+      <h2 className="text-3xl font-semibold mb-4 ">Order Successful!!</h2>
       <p>Your Order has been placed Successfully</p>
     </div>
   );
