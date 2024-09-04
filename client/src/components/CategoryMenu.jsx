98887ba88a8899952c47a2bd1c37f02e6e709ea5
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-04 20:37:11 +0530

    added onClick on button

--- client/src/components/CategoryMenu.jsx
+++ client/src/components/CategoryMenu.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import FoodData from "../data/foodData";
+import { useDispatch } from "react-redux";
+import { setCategory } from "../redux/slices/CategorySlices";
 
 const CategoryMenu = () => {
   const [categories, setCategories] = useState([]);
@@ -15,15 +17,15 @@ const CategoryMenu = () => {
     listUniqueCategories();
   }, []);
 
+  const dispatch = useDispatch();
+
   return (
     <div className="ml-6">
       <h3 className="text-xl font-semibold">Find the best Food</h3>
       <div className="my-5 flex gap-3 overflow-x-scroll scroll-smooth sm:overflow-x-hidden">
-        <button className="px-3 py-2 text-center bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white">
-          All
-        </button>
         {categories.map((category, index) => (
           <button
+            onClick={() => dispatch(setCategory(category))}
             key={index}
             className="px-3 py-2 text-center bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white"
           >
