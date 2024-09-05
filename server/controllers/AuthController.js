cd271050deb5be8005770ea3e79c2c372f507b7e
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-05 20:44:33 +0530

    added function for login

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -35,3 +35,43 @@ const signup = async (req, res) => {
     });
   }
 };
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+    if (!user)
+      return res.status(400).json({
+        success: false,
+        message: "User doesn't exist",
+      });
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch)
+      return res.status(400).json({
+        success: false,
+        message: "Wrong Password",
+      });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res
+      .cookie("cookie", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+      })
+      .status(200)
+      .json({
+        success: true,
+        message: "Login Successful",
+      });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
